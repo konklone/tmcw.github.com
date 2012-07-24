6bb67ca64c6cdbadfe187873688e7bb4ec483f76
Tom MacWright <tom@macwright.org>
2012-07-24 19:23:49 -0400

    IE

---
+++ js/ie.js
@@ -0,0 +1,4 @@
+if (!~~navigator.appVersion.indexOf("MSIE 7.")) {
+    alert("Hi, sorry, IE7, is the reason why the future doesn't happen.");
+    window.location = 'https://www.google.com/intl/en/chrome/browser/';
+}
