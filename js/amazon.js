056f70c39ae1b8e74d478633856e02dba8f6d323
Tom MacWright <tom@macwright.org>
2012-08-10 12:45:33 -0400

    Nah

--- js/amazon.js
+++
@@ -1,9 +0,0 @@
-window.setTimeout(function() {
-    var links = document.getElementsByTagName('A');
-    for (var i = 0; i < links.length; i++) {
-        var href = links[i].getAttribute('href');
-        if (href.indexOf('amazon.com') !== -1) {
-            links[i].setAttribute('href', href + '?tag=workingideala-20');
-        }
-    }
-}, 500);
